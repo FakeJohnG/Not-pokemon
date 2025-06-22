9e95bfce22001b1438f09882c4f1709cddc0b2cc
FakeJohnG <superjuan300@gmail.com>
2025-06-22 11:33:33 -0500

    Mini icono
    Ahora al ejecutar el juego el icono principal puede ser rempleazado por una imagen (como placeholder se usara por ahora grass2.png)

--- src/Notpokemon/Main.java
+++ src/Notpokemon/Main.java
@@ -1,14 +1,16 @@
 package Notpokemon;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 //Todo este codigo para que namas salga una ventana nueva, loco verdad?
+        ImageIcon icon= new ImageIcon(Main.class.getResource("/tiles/grass2.png"));
         JFrame window= new JFrame();
         GamePanel gamepanel= new GamePanel();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Not pokemon BETA");
+        window.setIconImage(icon.getImage());
         window.add(gamepanel);
         window.pack();
         window.setLocationRelativeTo(null);
